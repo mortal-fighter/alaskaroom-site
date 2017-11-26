e189afab91cf77314df36e27faed1e13f63cae6f
kirillvladykin <kirillmybox@rambler.ru>
2017-11-26 23:34:31 +0300

    end of day

--- routes/site/auth.js
+++ routes/site/auth.js
@@ -42,32 +42,30 @@ function getUserInfo() {
 	});
 }
 
-getUserInfo();
+//getUserInfo();
 
 router.get('/login_soc', function(req, res, next) {
 	res.redirect('https://oauth.vk.com/authorize?client_id=5763848&display=page&redirect_uri=http://localhost/auth/login_vk_callback&response_type=code&v=5.69');
 });
 
-router.all('/login_vk_callack', function(req, res, next) {
-	console.log(req.query);
-	res.send(req.query);
-
-	/*https://oauth.vk.com/access_token, передав следующие параметры: 
-
-client_id
-обязательный	Идентификатор Вашего приложения
-client_secret
-обязательный	Защищенный ключ Вашего приложения (указан в настройках приложения)
-redirect_uri
-обязательный	URL, который использовался при получении code на первом этапе авторизации. Должен быть аналогичен переданному при авторизации.
-code
-обязательный	Временный код, полученный после прохождения авторизации.*/
+router.get('/login_vk_callback', function(req, res, next) {
+	/*var options = {
+		uri: 'https://oauth.vk.com/access_token',
+		qs: {
+			client_id: '5763848',
+			code: '962d5238bb566af565',
+			client_secret: '9W8DQ2D3r97l3hWNoAfY',
+			redirect_uri: 'http://localhost/auth/login_vk_callback'
+		},
+		json: true
+	};*/
+	Promise.resolve().then(function() {
+		console.log(req.query.code);
+	});
 });
 
 function newSession() {
 
 };
-
-
 
 module.exports = router;
