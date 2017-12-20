a6dd4f971d3850ebb72cf52476700bf0566763f5
kirillvladykin <kirillmybox@rambler.ru>
2017-12-21 01:29:31 +0300

    - fix a bug with wrong sertificate

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/*
+[2017-12-21 01:22:46.331] [ERROR] [default] - Error: self signed certificate in certificate chain RequestError: Error: self signed certificate in certificate chain
+*/
+process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'; //todo: check it out whats going on
+
 const express = require('express');
 const app = express();
 const config = require('./config/common');
