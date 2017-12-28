12b2d87877ed1b64e77308f11e81d5fba336d726
kirillvladykin <kirillmybox@rambler.ru>
2017-12-28 11:28:12 +0300

    - remove unused files

--- config/menu.js
+++ config/menu.js
@@ -2,20 +2,6 @@ module.exports = {
 	menuAdmin: [
 	],
 	menuSite: [
-		{
-			titleShort: 'Главная',
-			titleFull: 'Главная страница',
-			titleCaption: 'Познакомимся',
-			href: '/',
-			hrefSecondary: '/',
-			topMenu: false 
-		}, 
-		{
-			titleShort: 'Объявления',
-			titleFull: 'Объявления',
-			titleCaption: 'Доска объявлений',
-			href: '/post',
-			topMenu: false
-		}
+		
 	]
 };
