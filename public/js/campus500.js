ff3a649970ef1e071b9047a2ba8989e15f6574ba
kirillvladykin <kirillmybox@rambler.ru>
2018-04-11 21:54:13 +0300

    Сделал страницу Кампус500 (только ее, нужно еще поправить профиль)

--- public/js/campus500.js
+++ public/js/campus500.js
@@ -4,9 +4,13 @@ var countRecords = 0;
 var lastForm = null;
 
 function campus500handlers() {
-	$('#load-more').on('click', function(e) {
+	$('.load-more').on('click', function(e) {
 		e.preventDefault();
 		loadMore();
+	});
+
+	$('#campus-option').on('change', function() {
+		search();
 	});
 
 	search();
@@ -27,34 +31,60 @@ function search() {
 			
 			if (result.status === 'ok') {
 				
-				countRecords = result.records.length;
+				var formattedRecords = [];
+				
+				result.users.forEach(function(user) {
+					formattedRecords.push({
+						id: user.id,
+						first_name: user.first_name,
+						last_name: user.last_name,
+						avatar: user.avatar,
+						campusOptions: []
+					});
+					
+					for (var i = 0; i < result.campusOptions.length; i++) {	
+						if (user.id === result.campusOptions[i].user_id) {
+							formattedRecords[formattedRecords.length - 1].campusOptions.push({
+								name: result.campusOptions[i].name,
+								icon: result.campusOptions[i].icon
+							});
+						}
+					}
+				});
+
+				//console.log('formattedRecords=', formattedRecords);
+
+				countRecords = formattedRecords.length;
 
 				var listing = $('#listing-content');
 				listing.html('');
 				
-				if (result.records.length === 0) {
-					listing.html('<div class="no-records">Поиск не дал результатов</div>');	
-					$('#load-more').hide();
-				} else if (result.records.length < result.recordsCountTotal ) {
-					$('#load-more').show();
-				} else {
-					$('#load-more').hide();
-				}
+				//console.log('result.usersCountTotal=', result.usersCountTotal);
 
-				for (var i = 0; i < result.records.length; i++) {
-					var record = result.records[i];
+				if (result.users.length === 0) {
+					listing.html('<div class="no-records">Поиск не дал результатов. Пожалуйста, зайдите попозже, мы найдем лучшего кандидата на эту роль.</div>');	
+					$('.load-more').hide();
+				} else if (formattedRecords.length < result.usersCountTotal ) {
+					$('.load-more').show();
+				} else {
+					$('.load-more').hide();
+				}
+				
+				for (var i = 0; i < formattedRecords.length; i++) {
+					var user = formattedRecords[i];
 					var newItem = null;
 					
 					newItem = $('<div class="user-list"></div>');
-					newItem.append($('<img src="'+ record.user_avatar +'" alt="User"/>'));
-					
+					newItem.append($('<img src="'+ user.avatar +'" alt="User"/>'));
+					newItem.append($('<h4>'+ user.first_name + '</h4>'));
+
 					var helpBox = $('<div class="help-box"></div>');
-					result.records.services.forEach(function(service) {
-						helpBox.append('<img src="' + service.icon + '", alt="' + service.name + '"');
+					user.campusOptions.forEach(function(option) {
+						helpBox.append('<img src="' + option.icon + '" alt="' + option.name + '" title="' + option.name + '" />');
 					});
 					newItem.append(helpBox);	
 
-					$('<a href="/profile/view/'+record.user_id+'"></a>')
+					$('<a href="/profile/view/'+user.id+'" title="Перейти на страницу пользователя ' + user.first_name + '"></a>')
 						.append(newItem)
 						.appendTo(listing);
 				}
@@ -69,7 +99,74 @@ function search() {
 }
 
 function loadMore() {
+	
+	lastForm.offset = countRecords;
 
+	$.ajax({
+		method: 'POST',
+		url: '/campus500/search',
+		dataType: 'json',
+		data: lastForm,
+		success: function(result) {
+			
+			if (result.status === 'ok') {
+				
+				var formattedRecords = [];
+				
+				result.users.forEach(function(user) {
+					formattedRecords.push({
+						id: user.id,
+						first_name: user.first_name,
+						last_name: user.last_name,
+						avatar: user.avatar,
+						campusOptions: []
+					});
+					
+					for (var i = 0; i < result.campusOptions.length; i++) {	
+						if (user.id === result.campusOptions[i].user_id) {
+							formattedRecords[formattedRecords.length - 1].campusOptions.push({
+								name: result.campusOptions[i].name,
+								icon: result.campusOptions[i].icon
+							});
+						}
+					}
+				});
+
+				countRecords += formattedRecords.length;
+
+				if (countRecords == result.usersCountTotal) {
+					$('.load-more').hide();
+				}
+
+				var listing = $('#listing-content');
+
+				for (var i = 0; i < formattedRecords.length; i++) {
+					var user = formattedRecords[i];
+					var newItem = null;
+					
+					newItem = $('<div class="user-list"></div>');
+					newItem.append($('<img src="'+ user.avatar +'" alt="User"/>'));
+					newItem.append($('<h4>'+ user.first_name + '</h4>'));
+
+					var helpBox = $('<div class="help-box"></div>');
+					user.campusOptions.forEach(function(option) {
+						helpBox.append('<img src="' + option.icon + '" alt="' + option.name + '" title="' + option.name + '" />');
+					});
+					newItem.append(helpBox);	
+
+					$('<a href="/profile/view/'+user.id+'" title="Перейти на страницу пользователя ' + user.first_name + '"></a>')
+						.append(newItem)
+						.appendTo(listing);
+				}
+
+			} else {
+				alert('При загрузке данных произошла ошибка');
+			}
+		},
+		error: function() {
+			alert('Проверьте соединение с Интернетом');
+		}
+	});
 }
 
 function processInputSelect(id, isSkipDefault) {
--- routes/site/campus500.js
+++ routes/site/campus500.js
@@ -8,7 +8,8 @@ const connectionPromise = require('../../components/connectionPromise.js');
 
 router.get('/', function(req, res, next) {
 	var db = null;
-	
+	var campus = [];
+
 	if (!req.isAuthorized) {
 		res.redirect('/?message=Пожалуйста авторизуйтесь на сайте');
 		return;
@@ -18,17 +19,14 @@ router.get('/', function(req, res, next) {
 
 		db = connection;
 
-		var sql = ` SELECT 
-						is_campus_payed
-					FROM user
-					WHERE id = ${req.user_id};`;
+		var sql = ` SELECT id, name FROM campus500 ORDER BY display_order;`;
 		logger.debug(req, sql);
 		return db.queryAsync(sql);
 
 	}).then(function(result) {
 
 		res.render('site/campus500.pug', {
-			is_campus_payed: result[0].is_campus_payed,
+			campus500: result,
 			isAuthorized: req.isAuthorized,
 			userId: req.user_id
 		});
@@ -42,35 +40,88 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/search', function(req, res, next) {
-	var db = null;
-	
 	if (!req.isAuthorized) {
 		res.json({
 			status: 'not ok',
-			message: 'Пожалуйста авторизуйтесь на сайте'
+			message: 'Пожалуйста войдите на сайт'
 		});
 		return;
 	}
+	
+	var db = null;
+	var countTotal = null;
+	var users = [];
+	
+	var limit = (req.body.limit) ? req.body.limit : 9;
+	var offset = (req.body.offset) ? req.body.offset : 0;
 
 	connectionPromise().then(function(connection) {
 
 		db = connection;
 
-		var sql = ` SELECT 
-						is_campus_payed
+		var sql = ` SELECT 	
+						user.id,
+						user.first_name,
+						user.last_name,
+						user.avatar
 					FROM user
-					WHERE id = ${req.user_id};`;
+					WHERE id IN (
+						SELECT DISTINCT user_id
+						FROM user_campus500
+						WHERE is_checked = 1
+						  AND campus500_id = ${req.body.campus.replace(/'/g, '\\\'')}
+					)
+					ORDER BY id DESC
+					LIMIT ${limit}
+					OFFSET ${offset};`;
 		logger.debug(req, sql);
 		return db.queryAsync(sql);
 
 	}).then(function(result) {
 
-		//
+		users = result;
+
+		var sql = `	SELECT COUNT(*) countTotal
+					FROM (
+						SELECT DISTINCT user_id
+						FROM user_campus500
+						WHERE is_checked = 1
+						  AND campus500_id = ${req.body.campus.replace(/'/g, '\\\'')}
+					) user_list;`;
+		logger.debug(req, sql);			
+		return db.queryAsync(sql);
+
+	}).then(function(result) {
 		
+		countTotal = result[0].countTotal;
+
+		var userIds = [];
+		users.forEach(function(user) { userIds.push(user.id); });
+
+		var sql = `	SELECT user_id, name, icon
+					FROM user_campus500
+					WHERE is_checked = 1
+					  AND user_id IN (${userIds.join(',')});`;
+
+		logger.debug(req, sql);
+		return db.queryAsync(sql);
+
+	}).then(function(result) {
+
+		res.json({
+			status: 'ok',
+			users: users,
+			campusOptions: result,
+			usersCountTotal: countTotal
+		});
+
 	}).catch(function(err) {
 
 		logger.error(err.message, err.stack);
-		res.render('errors/500.pug');
+		res.json({
+			status: 'not ok',
+			message: 'Во время выполнения запроса произошла ошибка'
+		});
 
 	});
 });
