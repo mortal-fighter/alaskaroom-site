3c8bf9c3be90a38a8da13098bdea8cde07be6f0b
kirillvladykin <kirillmybox@rambler.ru>
2018-05-08 11:28:59 +0300

    bugfix: страница Факультет

--- routes/site/faculty.js
+++ routes/site/faculty.js
@@ -2,15 +2,16 @@
 
 const router = require('express').Router();
 const Promise = require('bluebird');
-const logger = require('log4js').getLogger();
 
 const connectionPromise = require('../../components/connectionPromise.js');
+const logger = require('../../components/myLogger.js');
 
 router.post('/ajax_get_people_by_type', function(req, res, next) {
 
 	var db = null;
 	var records = [];
 	var recordsCountTotal = 0;
+	var conditionSql = null;
 
 	if (!req.isAuthorized) {
 		res.redirect('/?message=Пожалуйста,%20авторизуйтесь%20в%20системе');
@@ -24,90 +25,53 @@ router.post('/ajax_get_people_by_type', function(req, res, next) {
 
 		db = connection;
 
-		var sql = '';
-			
-			switch (req.body.type) {
-				case 'university':
-					sql = `	SELECT
-								user_id, user_first_name, user_last_name, user_sex, user_age, user_avatar, university_name 
-							FROM v_user
-							WHERE university_id = (SELECT university_id FROM v_user WHERE user_id = ${req.user_id})
-							  AND user_id <> ${req.user_id}
-							ORDER BY user_date_register DESC
-							LIMIT ${limit}
-							OFFSET ${offset};`;
-					break;
-				case 'faculty':
-					sql = ` SELECT
-								user_id, user_first_name, user_last_name, user_sex, user_age, user_avatar, university_name 
-							FROM v_user
-							WHERE faculty_id = (SELECT faculty_id FROM v_user WHERE user_id = ${req.user_id})
-							  AND user_id <> ${req.user_id}
-							ORDER BY user_date_register DESC
-							LIMIT ${limit}
-							OFFSET ${offset};`;
-					break;
-				case 'department':
-					sql = ` SELECT
-								user_id, user_first_name, user_last_name, user_sex, user_age, user_avatar, university_name 
-							FROM v_user
-							WHERE department_id = (SELECT department_id FROM v_user WHERE user_id = ${req.user_id})
-							  AND user_id <> ${req.user_id}
-							ORDER BY user_date_register DESC
-							LIMIT ${limit}
-							OFFSET ${offset};`;
-					break;
-				case 'studyyear':
-					sql = ` SELECT
-								user_id, user_first_name, user_last_name, user_sex, user_age, user_avatar, university_name 
-							FROM v_user
-							WHERE studyyear_id = (SELECT studyyear_id FROM v_user WHERE user_id = ${req.user_id})
-							  AND user_id <> ${req.user_id}
-							ORDER BY user_date_register DESC
-							LIMIT ${limit}
-							OFFSET ${offset};`;
-					break;
-			}
-		
-		logger.debug(sql);
+		switch (req.body.type) {
+			case 'university':
+				conditionSql = `university_id = (SELECT university_id FROM user WHERE id = ${req.user_id})`;
+				break;
+			case 'faculty':
+				conditionSql = `faculty_id = (SELECT faculty_id FROM user WHERE id = ${req.user_id})`;
+				break;
+			case 'department':
+				conditionSql = `department_id = (SELECT department_id FROM user WHERE id = ${req.user_id})`;
+				break;
+			case 'studyyear':
+				conditionSql = `studyyear_id = (SELECT studyyear_id FROM user WHERE id = ${req.user_id})`;
+				break;
 
+		}
+
+		var sql = `	SELECT
+						a.id 			user_id, 
+						a.first_name	user_first_name, 
+						a.last_name		user_last_name, 
+						a.sex			user_sex, 
+						a.age 			user_age, 
+						a.avatar 		user_avatar, 
+						b.name_short	university_name 
+					FROM user a
+					LEFT JOIN university b ON a.university_id = b.id
+					WHERE ${conditionSql}
+					  AND a.id <> ${req.user_id}
+					ORDER BY a.date_register DESC
+					LIMIT ${limit}
+					OFFSET ${offset};`;
+		
+		logger.debug(req, sql);
 		return db.queryAsync(sql);
 
 	}).then(function(result) {
 
-		logger.debug(result);
+		logger.debug(req, result);
 		records = result;
 
-		var sql = '';
-		switch (req.body.type) {
-			case 'university':
-				sql = ` SELECT count(*) cnt
-						FROM v_user
-						WHERE university_id = (SELECT university_id FROM v_user WHERE user_id = ${req.user_id})
-						  AND user_id <> ${req.user_id};`;
-				break;
-			case 'faculty':
-				sql = ` SELECT count(*) cnt
-						FROM v_user
-						WHERE faculty_id = (SELECT faculty_id FROM v_user WHERE user_id = ${req.user_id})
-						  AND user_id <> ${req.user_id};`;
-				break;
-			case 'department':
-				sql = ` SELECT count(*) cnt
-						FROM v_user
-						WHERE department_id = (SELECT department_id FROM v_user WHERE user_id = ${req.user_id})
-						  AND user_id <> ${req.user_id};`;
-				break;
-			case 'studyyear':
-				sql = ` SELECT count(*) cnt
-						FROM v_user
-						WHERE studyyear_id = (SELECT studyyear_id FROM v_user WHERE user_id = ${req.user_id})
-						  AND user_id <> ${req.user_id};`;
-				break;
-		}
+		var sql = `	SELECT count(*) cnt
+					FROM user a
+					LEFT JOIN university b ON a.university_id = b.id
+					WHERE ${conditionSql}
+					  AND a.id <> ${req.user_id};`;
 
-		logger.debug(sql);
-
+		logger.debug(req, sql);
 		return db.queryAsync(sql);
 
 	}).then(function(result) {
@@ -122,7 +86,7 @@ router.post('/ajax_get_people_by_type', function(req, res, next) {
 
 	}).catch(function(err) {
 
-		logger.error(err.message + ' ' + err.stack);
+		logger.error(req, err.message + ' ' + err.stack);
 
 		res.json({
 			status: 'not ok'
@@ -148,7 +112,7 @@ router.get('/', function(req, res, next) {
 
 	}).catch(function(err) {
 
-		logger.error(err.message + ' ' + err.stack);
+		logger.error(req, err.message + ' ' + err.stack);
 
 		res.render('errors/500.pug');
 
